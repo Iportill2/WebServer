cd145ddabe1bf4cacce6939142f1786b50f08b7c
Joseba Imanol Goikoetxea Otazua <jgoikoet@c4r2s1.42urduliz.com>
2024-06-05 12:27:20 +0200

    merge ok o no

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include "dependences.hpp"
+
+int main(int ac, char **av)
+{
+    if(ac== 1)
+        return Config(), 1;
+    else if(ac == 2)
+    {
+        std::string filename (av[1]);
+        return Config(filename), 1;
+    }
+    else
+        return std::cout << "Error: Ejecuta ./my_server o ./my_server server.conf" << std::endl, 0;
+}

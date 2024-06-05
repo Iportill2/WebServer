7b674c8aa4d0e695b35512bd63f43c3b2c1a4638
Joseba Imanol Goikoetxea Otazua <jgoikoet@c4r2s1.42urduliz.com>
2024-06-05 12:03:45 +0200

    main borrado

--- htons_ejemplo/htons.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <arpa/inet.h> // Para htons
-
-int main() {
-    unsigned short puerto_local = 8080;
-    unsigned short puerto_convertido = htons(puerto_local);//convertido es: 36895
-    std::cout << "Puerto local convertido a network byte order: " << puerto_convertido << std::endl;
-    return 0;
-}
